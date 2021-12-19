f878109bcae44219e2c58c5952129dca6343567c
Tovmasyan02 <armentovmasyan02@gmail.com>
2021-12-19 22:03:31 +0300

    added controllers

---
+++ src/main/java/com/sportShedule/controller/CategoryController.java
@@ -0,0 +1,40 @@
+package com.sportShedule.controller;
+
+import com.sportShedule.entity.Category;
+import com.sportShedule.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
+
+
+@RestController
+@RequestMapping("/category")
+public class CategoryController {
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @PostMapping
+    @ResponseBody
+    @ResponseStatus(code= HttpStatus.CREATED)
+    private Category create(@RequestBody Category category){
+        return categoryService.save(category);
+    }
+
+    @GetMapping("/{id}")
+    @ResponseBody
+    @ResponseStatus(code = HttpStatus.OK)
+    private Category findById(@PathVariable Long id){
+        return categoryService.findById(id);
+    }
+
+    @GetMapping("/all")
+    @ResponseBody
+    @ResponseStatus(code = HttpStatus.OK)
+    private Collection<Category> findAll(){
+        return categoryService.findAll();
+    }
+
+}
---
+++ src/main/java/com/sportShedule/controller/EventController.java
@@ -0,0 +1,45 @@
+package com.sportShedule.controller;
+
+import com.sportShedule.entity.Event;
+import com.sportShedule.service.EventService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
+
+@RestController
+@RequestMapping("/event")
+public class EventController {
+
+    @Autowired
+    private EventService eventService;
+
+    @PostMapping
+    @ResponseBody
+    @ResponseStatus(code= HttpStatus.CREATED)
+    private Event create(@RequestBody Event event){
+        return eventService.save(event);
+    }
+
+    @GetMapping("/{id}")
+    @ResponseBody
+    @ResponseStatus(code = HttpStatus.OK)
+    private Event findById(@PathVariable Long id){
+        return eventService.findById(id);
+    }
+
+    @GetMapping("/byCategoryId/{id}")
+    @ResponseBody
+    @ResponseStatus(code = HttpStatus.OK)
+    private Collection<Event> findByCategoryId(@PathVariable Long id){
+        return eventService.findAllByCategoryId(id);
+    }
+
+    @GetMapping("/all")
+    @ResponseBody
+    @ResponseStatus(code = HttpStatus.OK)
+    private Collection<Event> findAll(){
+        return eventService.findAll();
+    }
+}
